97057899501ba100def9a75fa83972ddf9e93395
alexkovalevv <alex.kovalevv@gmail.com>
2025-02-13 01:34:39 +0300

    Refactor Welcome page layout and content structure
    Replaced the existing layout with AppImpressionLayout to streamline design. Simplified the content, focusing on a minimalist information card with contact and social media links. Removed unused code and assets for better readability and maintainability.

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,30 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/fetch-title', function (Request $request) {
+    $url = $request->query('url');
+
+    if (!$url) {
+        return response()->json(['error' => 'URL is required'], 400);
+    }
+
+    try {
+        // Получаем содержимое страницы
+        $response = Http::get($url);
+
+        if ($response->successful()) {
+            // Парсим HTML и извлекаем заголовок
+            $html = $response->body();
+            preg_match('/<title>(.*?)<\/title>/s', $html, $matches);
+            $title = $matches[1] ?? 'No title found';
+
+            return response()->json(['title' => $title]);
+        } else {
+            return response()->json(['error' => 'Unable to fetch the URL'], $response->status());
+        }
+    } catch (\Exception $e) {
+        return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+    }
+});
+
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
             input: 'resources/js/app.js',
             refresh: true,
         }),
+
         vue({
             template: {
                 transformAssetUrls: {
@@ -17,4 +19,10 @@ export default defineConfig({
             },
         }),
     ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            '@img': path.resolve(__dirname, './resources/img'),
+        },
+    },
 });
