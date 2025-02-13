76f059b589408be9dff865e2b91fd971c1dc10e6
alexkovalevv <alex.kovalevv@gmail.com>
2025-02-13 03:21:27 +0300

    Add Puppeteer script for automated web page interaction
    Introduced a new script using Puppeteer to automate web interactions, including cookie acceptance and content extraction. Updated `package-lock.json` to include Puppeteer and related dependencies. This enhancement supports streamlined handling of web pages.

---
+++ resources/node-scripts/puppeteer.js
@@ -0,0 +1,40 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+    const args = process.argv.slice(2);
+    const url = args[0];
+
+    if (!url) {
+        console.error('URL is required');
+        process.exit(1);
+    }
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36');
+    await page.goto(url, {waitUntil: 'networkidle2'});
+
+    try {
+        // Ищем кнопку с атрибутом label="Принять все"
+        await page.waitForSelector('button[aria-label="Принять все"]', {timeout: 5000});
+        const [button] = await page.$$('button[aria-label="Принять все"]'); // На случай если кнопка вызывает новый переход
+        if (button) {
+            const navigationPromise = page.waitForNavigation({waitUntil: 'load', timeout: 10000}); // Ждем навигации
+            await button.click();
+            await navigationPromise; // Ожидаем завершение смены страницы
+            console.log('Cookie banner button clicked and navigation completed.');
+        }
+    } catch (error) {
+        console.log('No cookie banner button found or timed out.', error);
+    }
+
+    try {
+        const content = await page.content();
+        console.log(content);
+    } catch (error) {
+        console.error('Error fetching page content:', error);
+    }
+
+    await browser.close();
+})();
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FetchUrlController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,47 +8,5 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
-Route::get('/fetch-title', function (Request $request) {
-    $apiKey = 'AIzaSyCEKmua1S0TSp2NVCtjkU4E1tJVPXG1Zlg'; // Вставьте ваш API Key
-    $url = $request->query('url');
-
-    // Проверяем, есть ли канал в URL
-    if (!$url) {
-        return response()->json(['error' => 'URL is required'], 400);
-    }
-
-    // Извлекаем ID канала из URL
-    preg_match('/channel\/([a-zA-Z0-9_\-]+)/', $url, $matches);
-    $channelId = $matches[1] ?? null;
-
-    if (!$channelId) {
-        return response()->json(['error' => 'Invalid YouTube URL'], 400);
-    }
-
-    try {
-        // Формируем URL для API YouTube
-        $apiUrl = "https://www.googleapis.com/youtube/v3/channels?part=snippet&id={$channelId}&key={$apiKey}";
-
-        // Отправляем запрос к YouTube Data API
-        $response = Http::get($apiUrl);
-
-        if ($response->successful()) {
-            $data = $response->json();
-
-            var_dump($data);
-            exit;
-
-            // Получаем заголовок канала
-            $title = $data['items'][0]['snippet']['title'] ?? 'No title found';
-
-            return response()->json(['title' => $title]);
-        } else {
-            return response()->json(['error' => 'Unable to fetch channel information'], $response->status());
-        }
-    } catch (\Exception $e) {
-        return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
-    }
-});
-
+Route::get('/fetch-title', [FetchUrlController::class, 'fetchPageContent']);
 
